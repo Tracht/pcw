3a6b69004c284ce43cae3e6590a2b62a79605b2c
Tracht <marija.trachtenberg@gmail.com>
2021-04-21 16:07:06 +0100

    Adds css to Cards component

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,35 +1,48 @@
+import './cards.css'; 
+import noImg from '../assets/no-image-avail.jpg'
+
 const Cards = props => {
     const { results, latitude, longitude, distance } = props;
     
     return(
-        <div className="body">
+    <div>
+        <header>
+            <h4>{results && `${results.length} results`}</h4> 
+        </header>
 
-          <p>{results && `${results.length} results`}</p>
-
-          { 
-            results && <ul>
-              {
-              results && results.map(result => {
+        <div className="cards">
+            { results && results.map(result => {
                 return (
-                <div key={result.pageid}>
-                  <li>
-                    <p key={result.pageid + result.title}> {result.title} </p>
-                    <p key={result.pageid + result.title + 'distance'}> {result.coordinates ? `${distance(latitude, longitude, result.coordinates[0].lat, result.coordinates[0].lon, 'K')} km` : 'no distance information available'} </p>
+                    <article className="card" key={result.pageid}>
+                        <p key={result.pageid + result.title}>{result.title}</p> 
 
-                    <img key={result.pageid + result.title + 'img'} src={result.thumbnail && result.thumbnail.source} target="_blank"/> 
+                        {result.thumbnail && result.thumbnail.source 
+                            ?
+                            <img key={result.pageid + result.title + 'img'} 
+                                src={result.thumbnail && result.thumbnail.source}
+                                target="_blank"
+                                alt={'image of' + result.title}
+                            /> 
+                            : <img key={result.pageid + result.title + 'img'} 
+                                src={noImg} 
+                                target="_blank"
+                                alt={'No image available'}
+                                /> 
 
-                    <a key={result.pageid + result.title + 'imgURL'} href={result.thumbnail && result.thumbnail.source} target="_blank">
-                    {result.thumbnail ? '' : 'no picture available'}</a> <br></br>
+                        }
 
-                    <a key={result.pageid + result.title + 'url'} href={result.fullurl} target="_blank">learn more</a>
-                  </li> <br></br>
-                </div>
-                )
-              })
+                        <div classNamea="content">
+                            <p key={result.pageid + result.title + 'distance'}> {result.coordinates ? `${distance(latitude, longitude, result.coordinates[0].lat, result.coordinates[0].lon, 'K')} km` : 'no distance information available'} </p>
+
+                            <a key={result.pageid + result.title + 'url'} href={result.fullurl} target="_blank">learn more</a>
+                        </div>
+                    </article>
+                    )
+                })
             }
-            </ul> 
-          }
+
       </div>
+    </div>
     );
 };
 export default Cards;
